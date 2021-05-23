62931aad8192b5fc5e6ea38e4884c14fec0feb0b
qixiaojian310 <1821059387@qq.com>
2021-05-23 22:35:27 +0800

    corporate with team

--- src/main/java/sample/controllerImpl/LoginController.java
+++ src/main/java/sample/controllerImpl/LoginController.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import sample.Main;
 import sample.controller.ReadTextFieldable;
@@ -29,6 +30,9 @@ public class LoginController implements Initializable, ReadTextFieldable {
     @FXML
     private TextField password;
 
+    @FXML
+    private Label loginStatus;
+
     private User loginUser;
 
     public void setApp(Main application){
@@ -44,14 +48,22 @@ public class LoginController implements Initializable, ReadTextFieldable {
     }
 
     public User readTextField(){
+        boolean loginFlag = true;
         String usernameInfo = username.getText();
         String passwordInfo = password.getText();
         loginUser = new User();
         loginUser.setUsername(usernameInfo);
         loginUser.setPassword(passwordInfo);
+        if (loginFlag) {
+            loginStatus.setText("Wrong Password or not existed user");
+        }else{
+            application.gotoUserCenter();
+        }
         System.out.println(loginUser.toString());
         return loginUser;
     }
+
+    
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
