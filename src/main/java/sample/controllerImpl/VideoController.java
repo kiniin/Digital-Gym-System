b4ae319ccfd30b8ee0ea5f23fe062cd49a0f9bea
qixiaojian310 <1821059387@qq.com>
2021-05-25 20:51:39 +0800

    make a back button and add the lib of font

--- src/main/java/sample/controllerImpl/VideoController.java
+++ src/main/java/sample/controllerImpl/VideoController.java
@@ -12,11 +12,15 @@ import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Border;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import org.kordamp.ikonli.javafx.FontIcon;
 import sample.Main;
 import sample.simpleMediaPlayer.SimpleMediaPlayer;
 
@@ -26,13 +30,16 @@ public class VideoController implements Initializable{
 
   @FXML
   private GridPane videoBox;
+  @FXML
+  private Button backList;
+
 
   private SimpleMediaPlayer player;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     // TODO Auto-generated method stub
-
+    setFont();
   }
 
   public void setApp(Main application){
@@ -43,7 +50,7 @@ public class VideoController implements Initializable{
     // TODO 播放器自带的bug（当播放结束时不可以调进度了）
     // TODO 工具栏的布局bug
 //    player = SimpleMediaPlayer.newInstance(getClass().getResource("../video/TestMedia.mp4").toString(),693, 390);
-    player = SimpleMediaPlayer.newInstance("https://vd3.bdstatic.com/mda-mej527uefgtpm2f2/fhd/cae_h264_nowatermark/1621487341907760818/mda-mej527uefgtpm2f2.mp4?v_from_s=hkapp-haokan-tucheng&auth_key=1621885171-0-0-270b8c215f5b2703a46c59711fbd01ea&bcevod_channel=searchbox_feed&pd=1&pt=3&abtest=3000156_1",693, 390);
+    player = SimpleMediaPlayer.newInstance("https://vd3.bdstatic.com/mda-mej527uefgtpm2f2/fhd/cae_h264_nowatermark/1621487341907760818/mda-mej527uefgtpm2f2.mp4?v_from_s=hkapp-haokan-tucheng&auth_key=1621885171-0-0-270b8c215f5b2703a46c59711fbd01ea&bcevod_channel=searchbox_feed&pd=1&pt=3&abtest=3000156_1",693, 200);
     videoBox.getChildren().add(player);
     GridPane.setValignment(player, VPos.CENTER);
     GridPane.setHalignment(player, HPos.CENTER);
@@ -59,7 +66,7 @@ public class VideoController implements Initializable{
       @Override
       public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
         // TODO Auto-generated method stub
-        player.setMediaHeight((double)newValue*0.9);
+        player.setMediaHeight((double)newValue-41);
       }
       
     });
@@ -73,5 +80,16 @@ public class VideoController implements Initializable{
       }
       
     });
+  }
+
+  public void setFont(){
+    FontIcon homeIcon = new FontIcon("fa-home");
+    homeIcon.setIconSize(15);
+    homeIcon.setIconColor(new Color(1,1,1,1));
+    backList.setGraphic(homeIcon);
+  }
+
+  public void gotoVideoList(){
+    application.gotoVideoCenter();
   }
 }
