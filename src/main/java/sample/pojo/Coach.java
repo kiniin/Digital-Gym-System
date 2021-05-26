5aaf42ec919e60c87f886f2c9ecd4fe6267797e1
5Reasons <764432291@qq.com>
2021-05-27 00:44:34 +0800

    add pojo class:coach

---
+++ src/main/java/sample/pojo/Coach.java
@@ -0,0 +1,22 @@
+package sample.pojo;
+
+public class Coach {
+  String name;
+  String photoURL;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPhotoURL() {
+    return photoURL;
+  }
+
+  public void setPhotoURL(String photoURL) {
+    this.photoURL = photoURL;
+  }
+}
