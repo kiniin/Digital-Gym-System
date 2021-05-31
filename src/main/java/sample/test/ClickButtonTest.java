65cc6f1b47207ec6183e3b2563f9262735be3980
qixiaojian310 <1821059387@qq.com>
2021-05-31 17:17:45 +0800

    init Test package

---
+++ src/main/java/sample/test/ClickButtonTest.java
@@ -0,0 +1,11 @@
+import javafx.scene.control.Button;
+import org.testfx.framework.junit.ApplicationTest;
+
+public class ClickButtonTest extends ApplicationTest {
+
+    private Button InitButton;
+
+    private Button InitButton(){
+        return null;
+    }
+}
